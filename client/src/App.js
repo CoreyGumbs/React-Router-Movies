ea7c341f0c9e06f60131c223dc0c6140ac4973ad
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-12 17:59:08 -0600

    [Add] Imported Movie Component & Route to it using render props

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -14,7 +15,9 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path='/' component={MovieList} />
-      <Route />
+      <Route path='/movies/:id'  render={(props)=>(
+        <Movie {...props} savedList={addToSavedList} />
+      )}/>
     </div>
   );
 };
